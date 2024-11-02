6c75bd859fca80d4198e98b43c4916eae68e84b3
Enol <enol@burningroom.es>
2024-11-03 00:39:05 +0100

    actualizacion para deploy

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,27 +1,34 @@
 <?php
 // Configuración para mostrar todos los errores (útil en desarrollo)
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Obtener la URL de conexión desde la variable de entorno
+$mysql_url = getenv('MYSQL_URL');
 
-// Datos de conexión a la base de datos
+// Verificar que la URL esté disponible y extraer los detalles
+if ($mysql_url) {
+    $db = parse_url($mysql_url);
 
-$servername = "db5016593574.hosting-data.io";
-$username = "dbu199680";
-$password = "@Marerafols";
-$dbname = "dbs13455435";
-$port = 3306; // Especificar el puerto si es necesario
+    // Datos de conexión a la base de datos
+    $servername = $db['host'];
+    $username = $db['user'];
+    $password = $db['pass'];
+    $dbname = ltrim($db['path'], '/');
+    $port = $db['port'] ?? 3306; // Usar el puerto 3306 por defecto si no se especifica
 
+    // Crear conexión a la base de datos
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Crear conexión a la base de datos
-
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    // Verificar si la conexión es exitosa
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    } else {
+        echo "Conexión exitosa a la base de datos.";
+    }
+} else {
+    die("No se ha podido obtener la URL de conexión de la base de datos.");
 }
-
 
 
 
