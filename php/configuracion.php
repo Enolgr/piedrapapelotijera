f32da0759051c56ed65a2b2ec3b6e6f6599ddaf3
Enol <enol@burningroom.es>
2024-11-03 00:20:41 +0100

    conexion bbdd

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,15 +1,22 @@
 <?php
+// Configuración para mostrar todos los errores (útil en desarrollo)
 
-// Sirve para mostrar los errores.
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ppot";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Datos de conexión a la base de datos
+
+$servername = "db5016593574.hosting-data.io";
+$username = "dbu199680";
+$password = "@Marerafols";
+$dbname = "dbs13455435";
+$port = 3306; // Especificar el puerto si es necesario
+
+
+// Crear conexión a la base de datos
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
