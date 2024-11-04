53a68da1315a69d89f3e4aa988d638654eae4bc3
Enol <enol@burningroom.es>
2024-11-04 12:18:24 +0100

    railway app

--- php/configuracion.php
+++ php/configuracion.php
@@ -1,31 +1,15 @@
 <?php
-// Configuración para mostrar todos los errores (útil en desarrollo)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+session_start();
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
 
-// Obtener las variables de entorno
-$host = getenv('mysql.railway.internal'); // Dominio privado
-$port = getenv('11525'); // Puerto TCP
-$db   = 'ppot'; // Nombre de la base de datos
-$user = 'root'; // Usuario
-$pass = 'rYevINocCFMEoBZYIzpDbkXHEHNjMAlh'; // Contraseña
-$charset = 'utf8mb4'; // Conjunto de caracteres
+$db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
-// DSN para PDO
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Manejo de errores
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Modo de recuperación de datos
-    PDO::ATTR_EMULATE_PREPARES => false, // Usar preparaciones nativas
-];
-
-try {
-    // Crear la conexión
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "Conexión exitosa a la base de datos.";
-} catch (PDOException $e) {
-    // Manejo de excepciones
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 
